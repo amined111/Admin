83764577d212f959efbe59b595432c7c2b1dbd19
amined111 <43893689+amined111@users.noreply.github.com>
2018-12-03 01:16:44 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -3170,7 +3170,7 @@ channel.guild.owner.send(`<@!${channelremover.id}>
 }       });
 
 client.on('voiceStateUpdate', (old, now) => {
-  const channel = client.channels.get('516350860230524939');
+  const channel = client.channels.get('518943495419854854');
   const currentSize = channel.guild.members.filter(m => m.voiceChannel).size;
   const size = channel.name.match(/\[\s(\d+)\s\]/);
   if (!size) return channel.setName(`Voice Online: [ ${currentSize} ]`);
